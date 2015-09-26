e724726f1c53bc53b7269768143e1f49ddca8d55
Tarun Chattoraj <kumar.chattoraj@gmail.com>
2015-09-26 13:53:51 -0400

    checkpoint-27 completed

--- Gemfile
+++ Gemfile
@@ -61,3 +61,4 @@ gem 'turbolinks'
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   #gem 'spring'
 #end
+gem 'bootstrap-sass'
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,14 @@
 class WelcomeController < ApplicationController
   def index
+    # ...code
+    # @link = "baseball link"
   end
 
   def about
   end
+  #
+  # def create
+  #   post = Post.find 1
+  #   post.destroy
+  # end
 end
