23655d0f040cbea923332f0e40265b0fa150ef4b
Tarun Chattoraj <kumar.chattoraj@gmail.com>
2015-10-07 16:02:34 -0400

    checkpoint-36 completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,8 +26,25 @@ class PostsController < ApplicationController
     end
   end
 
-
-
   def edit
+    @post = Post.find(params[:id])
   end
+
+  def destroy
+    @post = Post.find(params[:id])
+
+    if @post.destroy
+      flash[:notice] = "\" #{@post.title}\" was deleted successfully."
+      redirect_to posts_path
+    else
+      flash[:error] = "There was an error deleting the post."
+      render :show
+    end
+
+  end
+
+
+
+
+
 end
