797eea3520d5bc84a2ad5497562deb1ad543e1f2
Tarun Chattoraj <kumar.chattoraj@gmail.com>
2015-10-20 22:36:39 -0400

    more DRY form_group_tag and Post validates allow_nil

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,7 @@
 module ApplicationHelper
 
   def form_group_tag(errors, &block)
-    if errors.any?
-      content_tag :div, capture(&block), class: 'form-group has-error'
-    else
-      content_tag :div, capture(&block), class: 'form-group'
+    extra_class = 'has-error' if errors.any?
+    content_tag :div, capture(&block), class: "form-group #{extra_class}"
     end
   end
-end
