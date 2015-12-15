8634aa2d3d745d647ff67737af0ba7e3f9fbbc46
Tarun Chattoraj <kumar.chattoraj@gmail.com>
2015-12-14 22:44:17 -0500

    checkpoint-49-api-completed

--- spec/api/v1/controllers/topics_controller_spec.rb
+++ spec/api/v1/controllers/topics_controller_spec.rb
@@ -12,7 +12,22 @@ RSpec.describe Api::V1::TopicsController, type: :controller do
 
     it "GET show returns http success" do
       get :show, id: my_topic.id
-      expect(response).to have-http_status(:success)
+      expect(response).to have_http_status(:success)
+    end
+
+    it "PUT update returns http unauthenticated" do
+      put :update, id: my_topic.id, topic: {name: "Topic Name", description: "Topic Description"}
+      expect(response).to have_http_status(401)
+    end
+
+    it "POST create returns http unathenticated" do
+      post :create, id: my_topic.id, topic: {name: "Topic Name", description: "Topic Description"}
+      expect(response).to have_http_status(401)
+    end
+
+    it "DELETE destroy returns http unathenticated" do
+      delete :destroy, id: my_topic.id
+      expect(response).to have_http_status(401)
     end
   end
 
@@ -29,6 +44,89 @@ RSpec.describe Api::V1::TopicsController, type: :controller do
     it "GET show returns http success" do
       get :show, id: my_topic.id
       expect(response).to have_http_status(:success)
+    end
+    it "PUT update returns http forbidden" do
+      put :update, id: my_topic.id, topic: {name: "Topic Name", description: "Topic Description"}
+      expect(response).to have_http_status(403)
+    end
+
+    it "POST create returns http forbidden" do
+      post :create, id: my_topic.id, topic: {name: "Topic Name", description: "Topic Description"}
+      expect(response).to have_http_status(403)
+    end
+
+    it "DELETE destroy returns http forbidden" do
+      delete :destroy, id: my_topic
+      expect(response).to have_http_status(403)
+    end
+  end
+
+  context "authenticated and authorized users" do
+    before do
+      my_user.admin!
+      controller.request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Token.encode_credentials(my_user.auth_token)
+      @new_topic = build(:topic)
+    end
+
+    describe "PUT update" do
+      before { put :update, id: my_topic.id, topic: {name: @new_topic.name, description: @new_topic.description}}
+
+      it "returns http success" do
+         expect(response).to have_http_status(:success)
+      end
+
+      it "returns json content type" do
+         expect(response.content_type).to eq 'application/json'
+      end
+
+      it "updates a topic with the correct attributes" do
+         updated_topic = Topic.find(my_topic.id)
+         expect(response.body).to eq updated_topic.to_json
+      end
+    end
+
+    describe "POST create" do
+
+      before {post :create, topic: {name: @new_topic.name, description: @new_topic.description}}
+
+
+      it "returns http success" do
+        expect(response).to have_http_status(:success)
+      end
+
+      it "returns json content type" do
+        post :create, topic: {name: @new_topic.name, description: @new_topic.description}
+        expect(response.content_type).to eq 'application/json'
+      end
+
+      it "creates a new topic having the given attributes" do
+        post :create, topic: {name: @new_topic.name, description: @new_topic.description}
+        hashed_json = JSON.parse(response.body)
+        expect(hashed_json["name"]).to eq @new_topic.name
+        expect(hashed_json["description"]).to eq @new_topic.description
+      end
+    end
+
+    describe "DELETE destroy" do
+      before do
+        @ident = my_topic.id
+        delete :destroy, id: my_topic.id
+      end
+
+
+      it "returns http success" do
+        expect(response).to have_http_status(:success)
+      end
+
+      it "returns json content type" do
+        expect(response.content_type).to eq 'application/json'
+      end
+
+      it "destroys the specified topic" do
+        deleted_topic_count = Topic.where(@ident).count
+        expect(deleted_topic_count).to eq 0
+        expect{Topic.find(my_topic.id)}.to raise_exception(ActiveRecord::RecordNotFound)
+      end
     end
   end
 end
--- spec/api/v1/controllers/users_controller_spec.rb
+++ spec/api/v1/controllers/users_controller_spec.rb
@@ -13,6 +13,18 @@ RSpec.describe Api::V1::UsersController, type: :controller do
       get :show, id: my_user.id
       expect(response).to have_http_status(401)
     end
+
+    it "PUT update returns http unauthenticated" do
+      new_user = build(:user)
+      put :update, id: my_user.id, user: {name: new_user.name, email: new_user.email, password: new_user.password}
+      expect(response).to have_http_status(401)
+    end
+
+    it "POST create returns http unauthenticated" do
+      new_user = build(:user)
+      post :create, id: my_user.id, user: {name: new_user.name, email: new_user.email, password: new_user.password}
+      expect(response).to have_http_status(401)
+    end
   end
 
   context "aunthenticated and unathorized users" do
@@ -22,6 +34,18 @@ RSpec.describe Api::V1::UsersController, type: :controller do
 
     it "GET index returns http forbidden" do
       get :index
+      expect(response).to have_http_status(403)
+    end
+
+    it "PUT update returns http forbidden" do
+      new_user = build(:user)
+      put :update, id: my_user.id, user: {name: new_user.name, email: new_user.email, password: new_user.password}
+      expect(response).to have_http_status(403)
+    end
+
+    it "POST create returns http forbidden" do
+      new_user = build(:user)
+      post :create, id: my_user.id, user: {name: new_user.name, email: new_user.email, password: new_user.password}
       expect(response).to have_http_status(403)
     end
   end
@@ -55,6 +79,87 @@ RSpec.describe Api::V1::UsersController, type: :controller do
 
       it "returns json content type" do
         expect(my_user.to_json).to eq response.body
+      end
+    end
+
+    describe "PUT update" do
+      context "with valid attributes" do
+        before do
+          @new_user = build(:user)
+          put :update, id: my_user.id, user: {name: @new_user.name, email: @new_user.email, password: @new_user.password, role: "admin"}
+        end
+
+        it "returns http success" do
+          expect(response).to have_http_status(:success)
+        end
+
+        it "returns json content type" do
+          expect(response.content_type).to eq 'application/json'
+        end
+
+        it "updates a user with the correct attributes" do
+          hashed_json = JSON.parse(response.body)
+          expect(hashed_json["name"]).to eq @new_user.name
+          expect(hashed_json["email"]).to eq @new_user.email
+          expect(hashed_json["role"]).to eq "admin"
+        end
+      end
+
+      context "with invalid attributes" do
+        before do
+          put :update, id: my_user.id, user: { name: "", email: "bademail@", password: "short"}
+        end
+
+        it "returns http error" do
+          expect(response).to have_http_status(400)
+        end
+
+        # Question:  Why do we get an "http error?"   We are authenticated and authorized so it seems we should get to the #update method.  Is it because the json response cannont be executed?
+
+        it "returns the correct json error message" do
+          expect(response.body).to eq({"error" => "User update failed", "status" => 400}.to_json)
+        end
+      end
+    end
+
+    describe "POST create" do
+      context "with valid attributes" do
+        before do
+          @new_user = build(:user)
+          post :create, id: my_user, user: {name: @new_user.name, email: @new_user.email, password: @new_user.password, role: "admin"}
+        end
+
+        it "returns http success" do
+          expect(response).to have_http_status(:success)
+        end
+
+        it "returns json content type" do
+          expect(response.content_type).to eq 'application/json'
+        end
+
+        it "creates a user with the correct attributes" do
+          @last_created_user = User.last
+          hashed_json = JSON.parse(response.body)
+          expect(hashed_json["name"]). to eq @last_created_user.name
+          expect(hashed_json["email"]). to eq @last_created_user.email
+          expect(hashed_json["role"]). to eq @last_created_user.role
+        end
+        #Note:  Checkpoint doesn't seem to be testing that a user is actually created--meaning saved, only that the #create action sets up the @new_user with the righ attributes.
+        #Also, seems to me the hashed_json[____] is what should be within the expect.  Might be symantics, but seems that we should "expect" the end result to be something not that something should be equal to the end result.
+      end
+
+      context "with invalid attributes" do
+        before do
+          post :create, user: {name: "", email: "bademail@", password: "short" }
+        end
+
+        it "retrns http error" do
+          expect(response).to have_http_status(400)
+        end
+
+        it "returns the correct json error message" do
+          expect(response.body).to eq({"error" => "User is invalid", "status" => 400}.to_json)
+        end
       end
     end
   end
